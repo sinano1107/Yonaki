23e3cdec96fbf96e8dbf6bd216789b4dad8653f7
百科おじさん <tftf161107@gmail.com>
2020-07-10 10:37:46 +0900

    [fix] next以外のメッセージも受け取れるように修正 - [add] unityからのログを表示する機能を追加

--- lib/services/unity_listener_service.dart
+++ lib/services/unity_listener_service.dart
@@ -1,18 +1,24 @@
+import 'dart:convert';
+
 class UnityListenerService {
   Function next = () => print('nextを指示されましたが何も設定されていません');
 
   void listen(String message) {
-    print(message);
-    switch (message) {
+    // 次へ移ってというメッセージ
+    if (message == 'next')
+      next();
+    else {
+      final program = jsonDecode(message);
+      switch (program['process']) {
+        // ログ
+        case 'log':
+          print('unityからのログ: ${program['log']}');
+          break;
 
-      // 次へ移ってというメッセージ
-      case 'next':
-        next();
-        break;
-
-      // エラー
-      default:
-        print('未知のUnityからのメッセージです。\nmessage: $message');
+        // エラー
+        default:
+          print('未知のUnityからのメッセージです。\nmessage: $message');
+      }
     }
   }
 }
